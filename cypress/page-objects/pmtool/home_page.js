b3e130d6530fb62e10681f727c1240283907fb24
Kateřina Lučná <katerina.lucna@gmail.com>
2024-04-02 20:59:45 +0200

    atomicke testy

--- cypress/page-objects/pmtool/home_page.js
+++ cypress/page-objects/pmtool/home_page.js
@@ -1,12 +1,10 @@
 import { HeaderSection } from "./common/header_section";
-import { LoginPage } from "./login_page";
-import { ProjectPage } from "./projects_page";
 
 export class HomePage extends HeaderSection {
   constructor() {
-    super();
+    super("module=dashboard/");
     this.welcomePageHeader = "#welcome-page-header";
-    cy.get(this.welcomePageHeader).should("be.visible");
+    //tato kontrola už tu taky nemůže být cy.get(this.welcomePageHeader).should("be.visible");
   }
 
   //v HomePage jsme po úpravě header_section a menu_section odmazali metodu a jeden this.//
