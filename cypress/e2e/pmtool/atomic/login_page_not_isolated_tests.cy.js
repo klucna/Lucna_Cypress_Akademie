dc3bae4af4c510eadb7a0d8be6d347268673bd8e
Kateřina Lučná <katerina.lucna@gmail.com>
2024-04-04 20:58:32 +0200

    atomic tests continues

---
+++ cypress/e2e/pmtool/atomic/login_page_not_isolated_tests.cy.js
@@ -0,0 +1,35 @@
+import { LoginPage } from "../../../page-objects/pmtool/login_page";
+
+describe("Login Page Atomic Tests", { testIsolation: false }, () => {
+  beforeEach(() => {
+    cy.clearAllCookies();
+    cy.clearAllLocalStorage();
+    cy.clearAllSessionStorage();
+    new LoginPage().openPmtool();
+  });
+
+  context("Username input tests", () => {
+    it("Username has placeholder", () => {
+      new LoginPage().usernameHavePlacelhoder("Username");
+    });
+
+    it("Username is visible", () => {
+      new LoginPage().usernameIsVisible();
+    });
+
+    it("Username has value after type", () => {
+      const username = "user";
+      new LoginPage().typeUsername(username).usernameHaveValue(username);
+    });
+  });
+
+  context("Password input tests", () => {
+    it("Password is visible", () => {
+      new LoginPage().passwordIsVisible();
+    });
+
+    it("Password has placeholder", () => {
+      new LoginPage().passwordHavePlacelhoder("Password");
+    });
+  });
+});
